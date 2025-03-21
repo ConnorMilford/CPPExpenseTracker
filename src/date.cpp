fa1a931ea4a5b237c1d2d0ecf3f118c335ae69c3
Connor <connormilford@hotmail.com>
2025-03-21 18:53:32 +0000

    Mosts tests passing on Test2 - Dates.

--- src/date.cpp
+++ src/date.cpp
@@ -6,25 +6,80 @@
 //
 // Canvas: https://canvas.swansea.ac.uk/courses/52781
 // -----------------------------------------------------
+#include <chrono>
+#include <string>
+#include <sstream>
+
 
 #include "date.h"
 
-// TODO: write a default constructor that sets the member variables to today's
+#define ARGUMENT_ERROR_MSG "Invalid argument from "
+
+
+// Destructor
+Date::~Date(){
+    // :-)
+}
+
+// A default constructor that sets the member variables to today's
 // date. 
 // Example:
 //  Date d = Date();
+Date::Date() {
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_t = std::chrono::system_clock::to_time_t(now);
+    std::tm* tmStruct = std::localtime(&now_t);
 
-// TODO: write a constructor that takes a three integer argument and sets the
+    this->year = tmStruct->tm_year + 1900;
+    this->month = tmStruct->tm_mon + 1;
+    this->day = tmStruct->tm_mday;
+}
+
+
+
+
+// A constructor that takes a three integer argument and sets the
 // appropriate member variables (year, month, day). If the date is not valid
 // throw an appropriate exception.
 // Example:
 //  Date d = Date("2024-12-25");
+
+Date::Date (unsigned int year, unsigned int month, unsigned int day) : year(year), month(month), day(day) { 
+    // :-)
+}
+
 
 // TODO: write a constructor that takes a string argument in "YYYY-MM-DD" format
 // and sets the appropriate member variables (year, month, day). If dateString
 // is not valid throw an appropriate exception.
 // Example:
 //  Date d = Date("2024-12-25");
+Date::Date(std::string date) {
+    std::stringstream dateStream(date);
+    std::string temp;
+
+    char delimiter = '-';
+
+    if (getline(dateStream, temp, delimiter)){
+        this->year = std::stoi(temp);
+    } else {
+        throw std::invalid_argument(ARGUMENT_ERROR_MSG + std::to_string(year));
+    }
+
+    if (getline(dateStream, temp, delimiter)){
+        this->month = std::stoi(temp);
+    } else {
+        throw std::invalid_argument(ARGUMENT_ERROR_MSG + std::to_string(year));
+    }
+
+    if (getline(dateStream, temp, delimiter)){
+        this->day = std::stoi(temp);
+    } else {
+        throw std::invalid_argument(ARGUMENT_ERROR_MSG + std::to_string(day));
+    }
+    
+
+}
 
 // TODO: Write a function, str, that takes no parameters and returns a
 // std::string representation of the Date object in YYYY-MM-DD format.  
@@ -44,17 +99,29 @@
 //  Date d = Date();
 //  auto year = d.getYear();
 
+unsigned int Date::getYear() const {
+    return this->year;
+}
+
 // TODO: Write a function, getMonth, that takes no parameters and returns month
 // member variable
 // Example:
 //  Date d = Date();
 //  auto month = d.getMonth();
 
+unsigned int Date::getMonth() const {
+    return this->month;
+}
+
 // TODO: Write a function, getDay, that takes no parameters and returns day
 // member variable
 // Example:
 //  Date d = Date();
 //  auto day = d.getDay();
+
+unsigned int Date::getDay() const {
+    return this->day;
+}
 
 // TODO: Write an == operator overload for the Date class, such that two
 // Date objects are equal only if they have the same member variables.
@@ -64,6 +131,15 @@
 //  if (d1 == d2) {
 //    ...
 //  }
+
+bool operator== (const Date &lhs, const Date &rhs) {
+    if (lhs.getYear() == rhs.getYear() &&
+        lhs.getMonth() == rhs.getMonth() &&
+        lhs.getDay() == rhs.getDay()) {
+            return true;
+        }
+        return false;
+}
 
 // TODO: Write an < operator overload for the Date class, that returns true if
 // the first Date object is chronologically before the second date object.
@@ -75,3 +151,26 @@
 //   if (d1 < d2) {
 //     ...
 //   }
+
+// checking if lhs before rhs
+bool operator<(const Date &lhs, const Date &rhs) {
+
+    if (lhs.getYear() < rhs.getYear()) {
+        return true;
+    } else if (lhs.getYear() > rhs.getYear()) {
+        return false;
+    }
+
+    if (lhs.getMonth() < rhs.getMonth()) {
+        return true;
+    } else if (lhs.getMonth() > rhs.getMonth()) {
+        return false;
+    }
+
+    return lhs.getDay() < rhs.getDay();
+}
+
+
+//std::ostream opeartor<<(const Date &date) {
+
+//}
--- src/date.h
+++ src/date.h
@@ -13,6 +13,33 @@
 // -----------------------------------------------------
 
 #ifndef DATE_H
+
 #define DATE_H
+
+class Date {
+    private:
+        unsigned int year;
+        unsigned int month;
+        unsigned int day;
+    public:
+        Date(unsigned int year, unsigned int month, unsigned int day);
+        Date(std::string date);
+        Date();
+        ~Date();    
+
+        unsigned int getYear() const;
+        unsigned int getMonth() const;
+        unsigned int getDay() const;
+
+        //friend std::ostream &operator<<(std::ostream &os, const Date& date);
+
+        friend bool operator==(const Date &lhs, const Date &rhs);
+
+        friend bool operator<(const Date &lhs, const Date &rhs);
+
+
+    };
+
+
 
 #endif // DATE_H
